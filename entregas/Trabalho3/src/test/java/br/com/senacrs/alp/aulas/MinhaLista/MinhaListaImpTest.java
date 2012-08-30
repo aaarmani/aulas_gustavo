803b164970845c66dd1373ca92c519a6894f56ec
armani <armani@gmail.com>
2012-08-30 17:19:14 -0300

    Introdução dos testes

--- entregas/Trabalho3/src/test/java/br/com/senacrs/alp/aulas/MinhaLista/MinhaListaImpTest.java
+++ entregas/Trabalho3/src/test/java/br/com/senacrs/alp/aulas/MinhaLista/MinhaListaImpTest.java
@@ -1,6 +1,7 @@
 package br.com.senacrs.alp.aulas.MinhaLista;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,37 +19,110 @@ public class MinhaListaImpTest {
 
 	@Test
 	public void testMinhaListaImp() {
-		fail("Not yet implemented");
+		try{
+		MinhaListaImp<Object> obj = new MinhaListaImp<Object>(null);
+		}catch(IllegalArgumentException e){
+			Assert.assertTrue(true);
+		}
 	}
 
 	@Test
 	public void testSufixar() {
-		fail("Not yet implemented");
+		MinhaListaImp<String> obj = new MinhaListaImp<String>("Inicio");
+		String esperado = "NovoInicio";
+		String resultado;
+		
+		obj.sufixar(esperado);
+		resultado = getSufixo(obj);
+		Assert.assertEquals(esperado, resultado);
+		
+	}
+
+	private String getSufixo(MinhaListaImp<String> obj) {
+		Nodo<String> nodo;
+		nodo = obj.getInicio();
+		
+		while(nodo.getProximo() != null){
+			nodo = nodo.getProximo();
+		}
+		
+		return nodo.getValor();
 	}
 
 	@Test
 	public void testPrefixar() {
-		fail("Not yet implemented");
+		MinhaListaImp<String> obj = new MinhaListaImp<String>("Inicio");
+		String esperado = "NovoInicio";
+		String resultado;
+		Nodo<String> nodo;
+		
+		obj.prefixar(esperado);
+		
+		nodo = obj.getInicio();
+		resultado = nodo.getValor();
+		
+		Assert.assertEquals(esperado, resultado);
 	}
 
 	@Test
 	public void testBuscar() {
-		fail("Not yet implemented");
+		Integer resultado;
+		Integer[] valor = new Integer[3];
+		valor[0] = 1;
+		valor[1] = 2;
+		valor[2] = 3;
+		MinhaListaImp<Integer> obj = new MinhaListaImp<Integer>(valor[0]);
+		
+		for(int i = 1; i < 3; i++){
+			obj.sufixar(valor[i]);
+		}
+
+		for(int i = 0; i < 3; i++){
+			resultado = obj.buscar(i);
+			Assert.assertEquals(valor[i], resultado);	
+		}
 	}
 
 	@Test
 	public void testInserir() {
-		fail("Not yet implemented");
+		MinhaListaImp<String> obj = new MinhaListaImp<String>("Inicio");
+		String valor = "Meio";
+		String resultado;
+		
+		obj.sufixar("Fim");
+		obj.inserir(1, valor);
+		
+		resultado = obj.buscar(1);
+		Assert.assertEquals(valor, resultado);
 	}
 
 	@Test
 	public void testRemover() {
-		fail("Not yet implemented");
+		MinhaListaImp<String> obj = new MinhaListaImp<String>("Inicio");
+		String valor = "Fim";
+		String resultado;
+		
+		obj.sufixar("Meio");
+		obj.sufixar(valor);
+		obj.remover(1); //remove o Meio
+		
+		resultado = obj.buscar(1);
+		Assert.assertEquals(valor, resultado);
 	}
 
 	@Test
 	public void testTamanho() {
-		fail("Not yet implemented");
+		final Integer qty = 5;
+		Integer resultado;
+		MinhaListaImp<Integer> obj = new MinhaListaImp<Integer>(0);
+		
+		for(int i = 1; i < qty; i++){
+			obj.sufixar(i);
+		}
+		
+		resultado = obj.tamanho();
+		
+		Assert.assertEquals(qty, resultado);
 	}
 
 }
