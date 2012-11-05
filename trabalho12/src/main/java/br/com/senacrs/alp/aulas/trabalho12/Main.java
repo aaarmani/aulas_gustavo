f524a3fba0962a6000a3006c6d3a8e380580150a
armani <armani@gmail.com>
2012-11-05 20:05:48 -0200

    Primeiro Commit do trabalho12

---
+++ trabalho12/src/main/java/br/com/senacrs/alp/aulas/trabalho12/Main.java
@@ -0,0 +1,13 @@
+package br.com.senacrs.alp.aulas.trabalho12;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ trabalho12/src/test/java/br/com/senacrs/alp/aulas/trabalho12/MainTest.java
@@ -0,0 +1,15 @@
+package br.com.senacrs.alp.aulas.trabalho12;
+
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+	
+	@Test
+	public void testCaminhoValido(){
+		
+	}
+
+}
